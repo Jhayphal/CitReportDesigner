25580f782cbda02382f26b5a1fbeb37430f1c32b
Oleg Nazarenko <olegn@devart.com>
2023-02-25 02:37:40 +0300

    Создал проект для анализа инструкций отчетов.

---
+++ ReportAnalizer/Program.cs
@@ -0,0 +1,39 @@
+﻿var folder = @"D:\VOPRJ8sp3\WinZ_16070R\BIN\REP";
+
+void ShowAll(string startsWith, string alias)
+{
+  var logFileName = @$"D:\Logs\{alias}.txt";
+
+  if (File.Exists(logFileName))
+  {
+    foreach (var line in File.ReadAllLines(logFileName))
+    {
+      Console.WriteLine(line);
+    }
+
+    return;
+  }
+
+  var instructions = new HashSet<string>();
+  foreach (var file in Directory.EnumerateFiles(folder!, "*.rep"))
+  {
+    foreach (var line in File.ReadAllLines(file))
+    {
+      if (line.StartsWith(startsWith))
+      {
+        instructions.Add(line);
+      }
+    }
+  }
+
+  var log = File.CreateText(logFileName);
+  foreach (var instruction in instructions)
+  {
+    log.WriteLine(instruction);
+    Console.WriteLine(instruction);
+  }
+}
+
+ShowAll("{/FL,", "FL");
+
+Console.ReadLine();
