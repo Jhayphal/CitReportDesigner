573237f281f079716491f88a80e59f2f50fe8119
Oleg Nazarenko <olegn@devart.com>
2023-02-24 23:18:42 +0300

    Покрыл тестами AfterStartParser.

---
+++ CitReport.IOTests/Parser/AfterStartParserTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CitReport.IO.Parser.Tests
+{
+  [TestClass()]
+  public class AfterStartParserTests
+  {
+    private readonly List<string> validCases = new()
+    {
+      "/AFTER START r_u := Azer(12)",
+      "/after start r_u := Azer(12)",
+      "/After Start  r_u := Azer(12) "
+    };
+
+    private readonly List<string> wrongCases = new()
+    {
+      "/REPORT repCode",
+      "/BLK repCode",
+      "{/*repCode*/}",
+      " /AFTER START r_u := Azer(12)"
+    };
+
+    private readonly TestErrorProvider errorProvider = new();
+    private readonly AfterStartParser parser = new();
+    private readonly ParserContext context;
+
+    public AfterStartParserTests()
+    {
+      context = new ParserContext(errorProvider);
+    }
+
+    [TestMethod()]
+    public void CanParseTest()
+    {
+      foreach (var testCase in validCases)
+      {
+        Assert.IsTrue(parser.CanParse(testCase, CodeContext.ReportDefinition));
+      }
+
+      foreach (var testCase in validCases)
+      {
+        Assert.IsFalse(parser.CanParse(testCase, CodeContext.CodeBehind));
+      }
+
+      foreach (var testCase in validCases)
+      {
+        Assert.IsFalse(parser.CanParse(testCase, CodeContext.Block));
+      }
+
+      foreach (var testCase in wrongCases)
+      {
+        Assert.IsFalse(parser.CanParse(testCase, CodeContext.ReportDefinition));
+      }
+    }
+
+    [TestMethod()]
+    public void Parse_Expression()
+    {
+      var expression = "R_U[1] := Substr(_NTrim(PST->YEAR), 3, 2) + StrZ(PST->MON, 2) + _NTrim(PST->PAKET)   ";
+      var testCase = $"/AFTER START   {expression}";
+
+      errorProvider.Errors.Clear();
+      parser.Parse(context, testCase);
+
+      var actual = context.Report.Definition.AfterStartActions.LastOrDefault();
+      var expected = new Expression { Value = expression };
+      
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
--- CitReport/Expression.cs
+++ CitReport/Expression.cs
@@ -1,6 +1,20 @@
 ﻿namespace CitReport;
 
-public class Expression
+public class Expression : IEquatable<Expression>
 {
   public string Value { get; set; }
+
+  public bool Equals(Expression other) => other is not null && Value == other.Value;
+
+  public override bool Equals(object obj) => Equals(obj as Expression);
+
+  public override int GetHashCode() => Value.GetHashCode();
+
+  public override string ToString() => Value;
+
+  public static bool operator ==(Expression left, Expression right) => left is null
+    ? right is null
+    : left.Equals(right);
+
+  public static bool operator !=(Expression left, Expression right) => !(left == right);
 }
