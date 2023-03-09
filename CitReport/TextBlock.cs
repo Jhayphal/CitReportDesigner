ae4e3e3f2468a6e92e46cfd68247be97b4092a9e
Oleg Nazarenko <olegn@devart.com>
2023-03-10 00:33:47 +0300

    Создал модельку для текстовых полей.

---
+++ CitReport/TextBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace CitReport;
+
+public class TextBlock
+{
+  public float X { get; set; }
+  
+  public float Y { get; set; }
+
+  public string Text { get; set; }
+
+  public float Width { get; set; }
+
+  public float Height { get; set; }
+
+  public HorizontalAlignment HorizontalAlignment { get; set; }
+
+  public VerticalAlignment VerticalAlignment { get; set; }
+
+  public Color ForegroundColor { get; set; } = Color.Black;
+
+  public Color BackgroundColor { get; set; } = Color.White;
+}
