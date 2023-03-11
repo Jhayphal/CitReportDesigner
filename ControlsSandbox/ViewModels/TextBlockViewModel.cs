cdf5e34e025309b034f4c6f4d946f26cbccbc2c4
Oleg Nazarenko <olegn@devart.com>
2023-03-11 19:28:17 +0300

    Добавил вью модель для текстовых блоков.

---
+++ ControlsSandbox/ViewModels/TextBlockViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Avalonia.Layout;
+using Avalonia.Media;
+using System;
+
+namespace ControlsSandbox.ViewModels
+{
+  public class TextBlockViewModel : ViewModelBase
+  {
+    private readonly CitReport.TextBlock textBlock;
+
+    public TextBlockViewModel(CitReport.TextBlock textBlock)
+    {
+      this.textBlock = textBlock;
+    }
+
+    public double X
+    {
+      get => textBlock.X;
+      set => textBlock.X = (float)value;
+    }
+
+    public float Y
+    {
+      get => textBlock.Y;
+      set => textBlock.Y = (float)value;
+    }
+
+    public string Text
+    {
+      get => textBlock.Text;
+      set => textBlock.Text= value;
+    }
+
+    public float Width
+    {
+      get => textBlock.Width;
+      set => textBlock.Width = (float)value;
+    }
+
+    public float Height
+    {
+      get => textBlock.Height;
+      set => textBlock.Height = (float)value;
+    }
+
+    public HorizontalAlignment HorizontalAlignment => ConvertHAligment(textBlock.HorizontalAlignment);
+
+    public VerticalAlignment VerticalAlignment => ConvertVAligment(textBlock.VerticalAlignment);
+
+    public IBrush ForegroundColor { get; set; } = Brushes.Black;
+
+    public IBrush BackgroundColor { get; set; } = Brushes.White;
+
+    private HorizontalAlignment ConvertHAligment(CitReport.HorizontalAlignment alignment) => alignment switch
+    {
+      CitReport.HorizontalAlignment.Center => HorizontalAlignment.Center,
+      CitReport.HorizontalAlignment.Right => HorizontalAlignment.Right,
+      CitReport.HorizontalAlignment.Left => HorizontalAlignment.Left,
+      _ => throw new InvalidOperationException(alignment.ToString())
+    };
+
+    private VerticalAlignment ConvertVAligment(CitReport.VerticalAlignment alignment) => alignment switch
+    {
+      CitReport.VerticalAlignment.Center => VerticalAlignment.Center,
+      CitReport.VerticalAlignment.Top => VerticalAlignment.Top,
+      CitReport.VerticalAlignment.Bottom => VerticalAlignment.Bottom,
+      _ => throw new InvalidOperationException(alignment.ToString())
+    };
+  }
+}
---
+++ ControlsSandbox/Views/TextBlockView.axaml.cs
@@ -0,0 +1,11 @@
+using ControlsSandbox.Controls;
+
+namespace ControlsSandbox.Views;
+
+public partial class TextBlockView : DraggableControl
+{
+    public TextBlockView()
+    {
+        InitializeComponent();
+    }
+}
